6ede55c3812f58211f08148b1050b23d4e11aa66
BARBARBoyyHD <helyxrulk@gmail.com>
2025-08-08 17:35:40 +0700

    fixing detail question

--- src/controllers/question/DetailQuestion.js
+++ src/controllers/question/DetailQuestion.js
@@ -3,14 +3,13 @@ import supabase from "../../supabase/supabase.js";
 export const detailQuestion = async (req, res) => {
   try {
     const { dt_id } = req.params;
-
     const { data, error } = await supabase
       .from("detail_level")
       .select(
         "dt_id, level_id, level_name, level_number, question, answer, user_id, created_at"
       )
       .eq("dt_id", dt_id)
-      .single();
+      .maybeSingle();
 
     if (error) throw error;
 
