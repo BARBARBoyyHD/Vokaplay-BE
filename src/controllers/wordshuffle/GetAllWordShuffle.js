eaceda62013ef155405f47f74ec915afff37a612
BARBARBoyyHD <helyxrulk@gmail.com>
2025-08-08 19:49:27 +0700

    fix : get all world shuffle

--- src/controllers/wordshuffle/GetAllWordShuffle.js
+++ src/controllers/wordshuffle/GetAllWordShuffle.js
@@ -2,10 +2,22 @@ import supabase from "../../supabase/supabase.js";
 
 export const getWordShuffle = async (req, res) => {
   try {
-
-    const { data, error } = await supabase
-      .from("word_shuffle")
-      .select("id, level_id, question, answer, question_number, asset_file");
+    const { data, error } = await supabase.from("word_shuffle").select(
+      `
+        id,
+        level_id,
+        question,
+        answer,
+        question_number,
+        asset_filet:asset_file (
+          asset_id,
+          asset_name,
+          asset_file_name,
+          created_at,
+          user_id
+        )
+      `
+    );
 
     if (error) throw error;
 
